671c586ad1adc3fe6ae0d1ca44ca0347af03d499
Razvan Joita <joitarazvan@yahoo.com>
2024-03-14 19:09:23 +0200

    Third Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,12 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-
         int x = 10;
         System.out.println(x);
+
+
+        String string = "Ana are mere";
+        System.out.println(string);
 
 
     }
